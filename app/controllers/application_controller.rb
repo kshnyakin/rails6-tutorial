c249d0f8f2cfdf853f1b8d8e9d7e0035da350d10
Alexander Kshnyakin <kshnyakin@uniweb.ru>
2021-04-28 23:24:27 +0300

    some progress, applicationController action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def hello
+    render html: "<h1>Hello, world</h1>"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'start#index'
+  root to: 'application#hello'
+  get '/start', to: 'start#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
